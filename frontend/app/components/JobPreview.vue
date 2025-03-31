93fb239df525135d472bf84329ef0f0cf29fbf71
Antoine <arotakerpro@gmail.com>
2025-03-31 22:40:29 +0200

    add nuxt with tailwind and prime vue

---
+++ frontend/app/components/JobPreview.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+  <div class="bg-surface-900 border border-surface-700 rounded-xl transition-all hover:-translate-y-1 shadow-primary-500/20 shadow-none hover:shadow-xl">
+    <div class="p-4 flex flex-row items-center">
+      <span class="font-bold font-lg">
+        Lorem ipsum dolor
+      </span>
+    </div>
+    <div class="p-4 pt-0">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </div>
+</template>
---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,13 @@
+<script>
+import Panel from 'primevue/panel';
+
+</script>
+
+<template>
+  <div class="p-8">
+    <div id="result-list" class="flex flex-col gap-6">
+      <JobPreview/>
+      <JobPreview/>
+    </div>
+  </div>
+</template>
